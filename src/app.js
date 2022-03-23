75435b13da7ce85ddec8404cea0b02a29160b1af
袁照阳 <alwaysmissly@163.com>
2022-03-23 20:51:40 +0800

    新增项目线上预览

--- src/app.js
+++ src/app.js
@@ -7,13 +7,46 @@ const routers = require('./routers')
 const { infoLogger, errorLogger } = require('./log')
 const { logTmp } = require('./utils/parserReq')
 const path = require('path')
+const proxy = require('koa2-proxy-middleware')
+const compress = require('koa-compress')
 
 // 服务
 const app = new Koa()
 
+// GZIP压缩
+app.use(
+  compress({
+    filter(content_type) {
+      return /\.(js|css|json)$/.test(content_type)
+    },
+    threshold: 10240,
+    gzip: {
+      flush: require('zlib').constants.Z_SYNC_FLUSH
+    },
+    deflate: {
+      flush: require('zlib').constants.Z_SYNC_FLUSH
+    },
+    br: true // disable brotli
+  })
+)
+
 // 配置静态web服务的中间件
-app.use(static(path.resolve(__dirname, './static/3dtiles/taizhou01')))
-app.use(static(path.resolve(__dirname, './static/gridJson')))
+app.use(static(path.resolve(__dirname, 'static/')))
+
+// 服务代理
+app.use(
+  proxy({
+    targets: {
+      '/api-yuhuan/(.*)': {
+        target: 'http://220.185.131.126:8190',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api-yuhuan': ''
+        }
+      }
+    }
+  })
+)
 
 // 解析请求体
 app.use(bodyparser({ enableTypes: ['json'] }))
